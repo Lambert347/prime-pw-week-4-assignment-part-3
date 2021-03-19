943a8f7f3c6330fdcd83ce7ca2b51acf9ffa226a
Ian Lambert <lambe347@umn.edu>
2021-03-19 12:22:25 -0500

    messing around with for of loops

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,30 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item) {
+  basket.push(item);
+  return true;
+}
+
+addItem('Bread');
+addItem('Cheese');
+addItem('Sandwich');
+addItem('Apple');
+
+
+function listItems() {
+  for (let i = 0; i < basket.length; i++) {
+    console.log(basket[i]);
+  }
+}
+
+function list() {
+  for (let item1 of basket) {
+    console.log(item1);
+  }
+}
+
+listItems();
+list();
