c5c27e48c082ac6406f3919cece9380e18c9799a
Ian Lambert <lambe347@umn.edu>
2021-03-19 15:30:23 -0500

    mostly done, check later for errors and fixes

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,20 +4,25 @@ console.log('***** Cart Functions *****');
 let basket = [];
 const maxItems = 5;
 
-
 function addItem(item) {
-  basket.push(item);
+  //Checks if the basket array is full by running the isFull function. If it is, then the addItem function prints the message that the basket is full and returns false.
   if (isFull() === true) {
-    console.log("Basket is full!")
+    console.log('Basket is full!')
     return false;
+  //If isFull returns false, then the addItem function adds the item to the array and returns true; signalling that the item has been added to the array.
   }
-  return true;.
+  basket.push(item);
+  return true;
 }
 
-addItem('Bread');
-addItem('Cheese');
-addItem('Sandwich');
-addItem('Apple');
+console.log(addItem('Bread'));
+console.log(addItem('Cheese'));
+console.log(addItem('Sandwich'));
+console.log(addItem('Apple'));
+console.log(addItem('Pasta'));
+console.log(addItem('Coffee'));
+//When checking the basket via console.log, coffee is not present because the basket is determined to be full. There are not enough spaces to stuff coffee beans in there.
+console.log(basket);
 
 
 function listItems() {
@@ -39,17 +44,33 @@ function empty() {
   basket = [];
 }
 
-empty();
+
+function empty2(array) {
+  array.length = 0;
+}
+
 console.log(basket);
 
 
 //STRETCH GOOOOOOOOOOOOOOOOOOOOOAAAAALS
 
 function isFull() {
-  if (basket.length < maxItems); {
-    return false;
-  } {
+  if (basket.length >= maxItems) {
     return true;
   }
+  return false;
 }
-console.log(isFull());
+
+
+//Function checks the basket if the item in the argument is present. If the item's index is -1, that means that the item is not present and the function will return null.
+function removeItem(item) {
+  if (basket.indexOf(item) === -1) {
+    return null;
+  }
+  //If the item in the argument is present, then the item will be removed from the array using splice.
+  basket.splice(item, 1);
+}
+
+//Function takes the argument Bread to remove it from the basket, and then console logs the new basket. The basket now has no bread in it.
+removeItem('Bread');
+console.log(basket);
